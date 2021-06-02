b507e2cbea7944097b043ab9a1ea33fee22d6f16
HajarePratik <pratikhajare09@gmail.com>
2021-06-02 12:10:14 +0530

    Showing Employee is Present or Not with Wage

--- Empwage.java
+++ Empwage.java
@@ -5,15 +5,24 @@ class Empwage
 		System.out.println("Welcome to the Employee Wage Computation Program");
 		
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		int empHour = 0;
+		int empWage = 0;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
 		if(empCheck == IS_FULL_TIME)
 		{
 				System.out.println("Employee is Present");
+				empHour = 8 ;
 		}
 		else
 		{
-			System.out.println("Employee is Absent");
+				System.out.println("Employee is Absent");
+				empHour = 0;
 		}
+		empWage = empHour * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage is :" + empWage);
 	}
 }
