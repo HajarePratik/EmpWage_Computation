69b3f901ccb971e6fa47fa10c9ce97d198ce3bf5
HajarePratik <pratikhajare09@gmail.com>
2021-06-02 12:22:41 +0530

    Showing Employee is Wage with FullTime or PartTime

--- Empwage.java
+++ Empwage.java
@@ -5,17 +5,23 @@ class Empwage
 		System.out.println("Welcome to the Employee Wage Computation Program");
 		
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		
 		int empHour = 0;
 		int empWage = 0;
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		
 		if(empCheck == IS_FULL_TIME)
 		{
-				System.out.println("Employee is Present");
+				System.out.println("Employee is Present & Doing Full Time");
 				empHour = 8 ;
+		}
+		else if(empCheck == IS_PART_TIME)
+		{
+			System.out.println("Employee is Present & Doing Part Time");
+			empHour = 4;
 		}
 		else
 		{
