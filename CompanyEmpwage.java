59ee14fb44976bba7bc6da901e52454e1c8ad3cf
PratikHajare <66741672+HajarePratik@users.noreply.github.com>
2021-06-03 11:45:44 +0530

    Update CompanyEmpwage.java

--- CompanyEmpwage.java
+++ CompanyEmpwage.java
@@ -1,12 +1,14 @@
 
 public class CompanyEmpwage {
 
+	//Declearing Varibles
 	public final String company;
 	public final int empRatePerHour;
 	public final int numOfWorkingDays;
 	public final int maxHoursPerMonth;
 	public int totalEmpWage = 0;
 	
+	//Creating a Constuctor here
 	public CompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
 	{
 		this.company = company;
@@ -14,7 +16,7 @@ public class CompanyEmpwage {
 		this.numOfWorkingDays = numOfWorkingDays;
 		this.maxHoursPerMonth = maxHoursPerMonth;
 	}
-
+	// set TotalWage Variable to the Method
 	public void setTotalEmpWage(int totalEmpWage) {
 		this.totalEmpWage = totalEmpWage;
 	}
