c60f5a1a4ae8e0df4d333c4eb68e0e5160301aaa
PratikHajare <66741672+HajarePratik@users.noreply.github.com>
2021-06-03 11:59:28 +0530

    Update InterfaceEmpwage.java

--- InterfaceEmpwage.java
+++ InterfaceEmpwage.java
@@ -1,9 +1,9 @@
 
 public interface InterfaceEmpwage {
 	
-	public void addCompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
+	public void addCompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth); // Declearing addCompany Method in Interface
 	
-	public void computeEmpWage();
-	public int getTotalWage(String company);
+	public void computeEmpWage(); 			 // Declearing computeEmpWage Method in Interface
+	public int getTotalWage(String company);	// Declearing a Method here with Passing a One Parameter is Company Name
 
 }
