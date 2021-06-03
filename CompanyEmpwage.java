14959e0950ea2ec661cc7e4a953b15d8d901424d
PratikHajare <66741672+HajarePratik@users.noreply.github.com>
2021-06-03 11:52:46 +0530

    Update CompanyEmpwage.java

--- CompanyEmpwage.java
+++ CompanyEmpwage.java
@@ -2,14 +2,16 @@ import java.util.ArrayList;
 
 public class CompanyEmpwage {
 
+	//Declearing Varibles
 	public final String company;
 	public final int empRatePerHour;
 	public final int numOfWorkingDays;
 	public final int maxHoursPerMonth;
 	public int totalEmpWage = 0;
 	
-	public ArrayList<Integer> empDailyWage = new ArrayList<Integer>();
+	public ArrayList<Integer> empDailyWage = new ArrayList<Integer>();   // Here Initailizing a IntegerType of Arraylist
 	
+	//Creating a Constuctor here
 	public CompanyEmpwage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth)
 	{
 		this.company = company;
@@ -18,6 +20,7 @@ public class CompanyEmpwage {
 		this.maxHoursPerMonth = maxHoursPerMonth;
 	}
 
+	// set TotalWage Variable to the Method
 	public void setTotalEmpWage(int totalEmpWage) {
 		this.totalEmpWage = totalEmpWage;
 	}
